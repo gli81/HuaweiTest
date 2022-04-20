c6171d75b41458d8b1a9041e88479e305e77bd46
gli81 <liguangzheng998@hotmail.com>
2022-04-21 00:24:51 +0800

    added

--- 014/WordRank.py
+++ 014/WordRank.py
@@ -2,11 +2,11 @@
 
 class WordRank:
     def wordRank(self, words:'list[str]') -> 'list[str]':
-        pass
+        return sorted(words)
 
 def main():
     test = WordRank()
-    test.wordRank()
+    print(test.wordRank(["cap", "to", "cat", "card", "two", "too", "up", "boat", "boot"]))
 
 if __name__ == '__main__':
     main()
