ddb23dc231f768b28b442a4ce59d02049e4f450c
gli81 <liguangzheng998@hotmail.com>
2022-04-20 23:53:59 +0800

    Added new files

---
+++ 014/WordRank.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class WordRank:
+    def wordRank(self, words:'list[str]') -> 'list[str]':
+        pass
+
+def main():
+    test = WordRank()
+    test.wordRank()
+
+if __name__ == '__main__':
+    main()
