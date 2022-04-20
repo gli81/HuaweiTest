8ec7d0bb86f5fa7c25d932c6ba148879f01d731d
gli81 <liguangzheng998@hotmail.com>
2022-04-21 00:37:46 +0800

    added 015 solution

--- 015/Decimal2Binary.py
+++ 015/Decimal2Binary.py
@@ -2,12 +2,21 @@
 
 class Decimal2Binary:
     def decimal2Binary(self, num:'int') -> 'int':
-        pass
+        digits = []
+        while num >= 1:
+            digits.append(num % 2)
+            num = num // 2
+        ct = 0
+        for digit in digits:
+            if digit == 1:
+                ct += 1
+        return ct
 
 def main():
     test = Decimal2Binary()
     print(test.decimal2Binary(5))
     print(test.decimal2Binary(0))
+    print(test.decimal2Binary(11))
 
 if __name__ == '__main__':
     main()
