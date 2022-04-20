306dd59f1a07583ca694e49cff38baf260dd3529
gli81 <liguangzheng998@hotmail.com>
2022-04-21 00:31:03 +0800

    added 015

---
+++ 015/Decimal2Binary.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class Decimal2Binary:
+    def decimal2Binary(self, num:'int') -> 'int':
+        pass
+
+def main():
+    test = Decimal2Binary()
+    print(test.decimal2Binary(5))
+    print(test.decimal2Binary(0))
+
+if __name__ == '__main__':
+    main()
