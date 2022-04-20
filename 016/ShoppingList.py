21f2ba15542ff586e0b65c66b8f101292d925388
gli81 <liguangzheng998@hotmail.com>
2022-04-21 03:51:53 +0800

    added 016

--- 016/ShoppingList.py
+++ 016/ShoppingList.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
 
 class ShoppingList:
-    def shoppingList(self, items: 'dict[int,int]') -> 'int':
+    def shoppingList(self, items: 'dict[int,int]', balance: 'int', count:'int') -> 'int':
         pass
 
 def main():
     test = ShoppingList()
-    print(test.shoppingList)
+    print(test.shoppingList({800:2}, 1000, 5))
 
 if __name__ == '__main__':
     main()
