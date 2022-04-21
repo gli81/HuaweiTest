cb3cc522cc4e16fa5a037e7e72585c24093bf4af
gli81 <liguangzheng998@hotmail.com>
2022-04-22 07:01:03 +0800

    added 024

--- 023/DeleteChar.py
+++ 023/DeleteChar.py
@@ -2,9 +2,22 @@
 
 class DeleteChar():
     def deleteChar(self, line: 'str') -> 'str':
-        pass
+        char_map = {}
+        for char in line:
+            if char in char_map: char_map[char] += 1
+            else: char_map[char] = 1
+        min_apperance = min(char_map.values())
+        ans = ""
+        for char in line:
+            if char_map[char] != min_apperance:
+                ans += char
+        return ans
+
 
 def main():
     test = DeleteChar()
     print(test.deleteChar("aabcddd"))
     print(test.deleteChar("aabcddde"))
+
+if __name__ == "__main__":
+    main()
---
+++ 024/Chorus.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Chorus():
+    def chorus(self, height: 'list[int]') -> 'int':
+        pass
+
+def main():
+    test = Chorus()
+    print(test.chorus([186, 186, 150, 200, 160, 130, 197, 200]))
+
+if __name__ == "__main__":
+    main()
