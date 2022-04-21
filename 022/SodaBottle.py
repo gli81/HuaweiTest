d1b8a3fa8d6a63a5a27bd20afaf67a2e8ba1293e
gli81 <liguangzheng998@hotmail.com>
2022-04-22 06:47:14 +0800

    added 023

--- 022/SodaBottle.py
+++ 022/SodaBottle.py
@@ -2,7 +2,9 @@
 
 class SodaBottle:
     def sodaBottle(self, num: 'int') -> 'int':
-        pass
+        return int(num / 2)
+        ## get one with 2 empty soda bottles, borrow another empty one from the shop
+        ## exchange for a new one, drink it and return.
 
 def main():
     pass
---
+++ 023/DeleteChar.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+
+class DeleteChar():
+    def deleteChar(self, line: 'str') -> 'str':
+        pass
+
+def main():
+    test = DeleteChar()
+    print(test.deleteChar("aabcddd"))
+    print(test.deleteChar("aabcddde"))
