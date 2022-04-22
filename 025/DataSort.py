0dc591b8854eafc079337c10e53fee9ce94d6035
gli81 <liguangzheng998@hotmail.com>
2022-04-22 11:48:08 +0800

    added 025

---
+++ 025/DataSort.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class DataSort():
+    def dataSort(self, l: 'list[int]', r: 'list[int]') -> 'list[int]':
+        pass
+
+def main():
+    test = DataSort()
+    print(test.dataSort())
+
+if __name__ == "__main__":
+    main()
