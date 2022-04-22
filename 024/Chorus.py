5d68a764aedafb16aa0d7e24dae58fb652456ebb
gli81 <liguangzheng998@hotmail.com>
2022-04-22 10:15:20 +0800

    modified 024

--- 024/Chorus.py
+++ 024/Chorus.py
@@ -1,12 +1,14 @@
 # -*- coding: utf-8 -*-
 
 class Chorus():
-    def chorus(self, height: 'list[int]') -> 'int':
+    def chorusDP(self, heights: 'list[int]') -> 'int':
+        pass
+    def dp(self, heights: 'list[int]') -> 'list[int]':
         pass
 
 def main():
     test = Chorus()
-    print(test.chorus([186, 186, 150, 200, 160, 130, 197, 200]))
+    print(test.chorusDP([186, 186, 150, 200, 160, 130, 197, 200]))
 
 if __name__ == "__main__":
     main()
