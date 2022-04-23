fd4466edd57192383ef41da06095cd86624b0b67
gli81 <liguangzheng998@hotmail.com>
2022-04-24 05:44:15 +0800

    modified 031

--- 031/ReverseLine.py
+++ 031/ReverseLine.py
@@ -1,22 +1,22 @@
 # -*- coding: utf-8 -*-
 
 class ReverseLine():
-	def reverseLine(self, line: 'str'):
-		for char in line:
-			if not char.isalpha():
-				line = line.replace(char, ' ', 1)
-		splited = line.split()
-		ans = ''
-		for i in range(len(splited) - 1, -1, -1):
-			ans += splited[i]
-			if i != 0:
-				ans += ' '
-		return ans
+    def reverseLine(self, line: 'str'):
+        for char in line:
+            if not char.isalpha():
+                line = line.replace(char, ' ', 1)
+        splited = line.split()
+        ans = ''
+        for i in range(len(splited) - 1, -1, -1):
+            ans += splited[i]
+            if i != 0:
+                ans += ' '
+        return ans
 
 def main():
-	test = ReverseLine()
-	print(test.reverseLine("I am a student"))
-	print(test.reverseLine("$bo*y gi!r#l"))
+    test = ReverseLine()
+    print(test.reverseLine("I am a student"))
+    print(test.reverseLine("$bo*y gi!r#l"))
 
 if __name__ == "__main__":
-	main()
+    main()
