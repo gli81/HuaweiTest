3949452269d995caa0d1c29339a969d21c57a7b1
gli81 <liguangzheng998@hotmail.com>
2022-04-24 05:44:11 +0800

    modified 031

--- 029/StrEncode.py
+++ 029/StrEncode.py
@@ -8,6 +8,10 @@ class StrEncode():
                 ans += chr(ord(char) - 31)
             elif char == 'z':
                 ans += 'A'
+            elif ord(char) in range(65, 90):
+                ans += chr(ord(char) + 33)
+            elif char == 'Z':
+                ans += 'a'
             elif ord(char) in range(48, 57):
                 ans += chr(ord(char) + 1)
             elif char == '9':
@@ -21,6 +25,10 @@ class StrEncode():
                 ans += chr(ord(char) + 31)
             elif char == 'A':
                 ans += 'z'
+            elif ord(char) in range(98, 123):
+                ans += chr(ord(char) - 33)
+            elif char == 'a':
+                ans += 'Z'
             elif ord(char) in range(49, 58):
                 ans += chr(ord(char) - 1)
             elif char == '0':
@@ -31,6 +39,8 @@ def main():
     test = StrEncode()
     print(test.encode("abcdefgz0129"))
     print(test.decode("BCDEFGHA1230"))
+    print(test.encode("2OA92AptLq5G1lW8564qC4nKMjv8C"))
+    print(test.decode("B5WWIj56vu72GzRja7j5"))
 
 if __name__ == "__main__":
     main()
