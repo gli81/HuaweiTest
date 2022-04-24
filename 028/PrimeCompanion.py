6461106b85b55826001674c858b6bf82b2f5f850
gli81 <liguangzheng998@hotmail.com>
2022-04-24 09:50:41 +0800

    added 028

---
+++ 028/PrimeCompanion.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+class PrimeCompanion():
+    def primeCompanion(self, nums: 'list[int]') -> 'int':
+        pass
+
+def main():
+    test = PrimeCompanion()
+
+if __name__ == "__main__":
+    main()
