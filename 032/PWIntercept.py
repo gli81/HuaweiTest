63182a574e1afa28ad91eb1ed69fac3cb084cef6
gli81 <liguangzheng998@hotmail.com>
2022-04-24 19:18:30 +0800

    added 033

--- 032/PWIntercept.py
+++ 032/PWIntercept.py
@@ -2,7 +2,26 @@
 
 class PWIntercept():
     def pWIntercept(self, pw: 'str') -> 'int':
-        pass
+        ans = 0
+        for i in range(len(pw)):
+            len1 = self.palindromeCenteredAt(pw, i, i)
+            len2 = self.palindromeCenteredAt(pw, i, i + 1)
+            length = max(len1, len2)
+            if length > ans:
+                ans = length
+        return ans
+        
+    def palindromeCenteredAt(self, s: 'str', leftIndex: 'int', rightIndex: 'int') -> 'int':
+        '''
+        LeetCode 005
+        '''
+        left = leftIndex
+        right = rightIndex
+        while left >= 0 and right < len(s) and s[left] == s[right]:
+            left -= 1
+            right += 1
+        return right - left - 2 + 1
+
 
 def main():
     test = PWIntercept()
---
+++ 033/IntegerIP.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class IntergerIP():
+    def integerIP(self, ip: 'str') -> 'str':
+        pass
+
+def main():
+    test = IntergerIP()
+    print(test.integerIP())
+
+if __name__ == "__main__":
+    main()
