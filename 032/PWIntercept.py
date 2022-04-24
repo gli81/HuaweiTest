449c8d7cbefd532aec68cb45d0fae28d1339cd4b
gli81 <liguangzheng998@hotmail.com>
2022-04-24 18:54:49 +0800

    added 032

---
+++ 032/PWIntercept.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+class PWIntercept():
+    def pWIntercept(self, pw: 'str') -> 'int':
+        pass
+
+def main():
+    test = PWIntercept()
+    print(test.pWIntercept("ABBA"))
+    print(test.pWIntercept("ABBBA"))
+
+if __name__ == "__main__":
+    main()
+
