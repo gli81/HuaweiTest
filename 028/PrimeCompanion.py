e931bcee626c3c5a9c4ebb79571802d9ec0966b2
gli81 <liguangzheng998@hotmail.com>
2022-04-24 10:04:37 +0800

    edited 028

--- 028/PrimeCompanion.py
+++ 028/PrimeCompanion.py
@@ -3,6 +3,13 @@
 class PrimeCompanion():
     def primeCompanion(self, nums: 'list[int]') -> 'int':
         pass
+    def isPrime(self, num: 'int'):
+        if num == 1:
+            return False
+        for i in range(2, int(num ** 0.5) + 1):
+            if num % i == 0:
+                return False
+        return True
 
 def main():
     test = PrimeCompanion()
