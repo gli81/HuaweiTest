cce820d3247d7d75e7a9a2481cffa62b2f3fa797
gli81 <liguangzheng998@hotmail.com>
2022-04-24 18:51:57 +0800

    completed 030

--- 030/ConcatStr.py
+++ 030/ConcatStr.py
@@ -2,6 +2,7 @@
 
 class ConcatStr():
     def concatStr(self, str1: 'str', str2: 'str') -> 'str':
+        ans = ''
         first_step = str1 + str2
         odds = []
         evens = []
@@ -17,8 +18,14 @@ class ConcatStr():
         # print(evens, odds)
         for i in range(len(evens)):
             second_step = second_step + evens[i] + odds[i]
+        # print(second_step)
         for char in second_step:
-            print(self.hex2Bin(char))
+            # print(self.hex2Bin(char))
+            if char.isdigit() or (char.isalpha() and char.upper() < 'G'):
+                ans += self.bin2Hex(self.hex2Bin(char)[::-1])
+            else:
+                ans += char
+        return ans
 
     def hex2Bin(self, num: 'str') -> 'str':
         hex_dec_dict = {'a':10, 'b':11, 'c':12, 'd':13, 'e':14, 'f':15}
@@ -41,24 +48,47 @@ class ConcatStr():
 
     def bin2Hex(self, num: 'str') -> 'str':
         dec_hex_dict = {10: 'A', 11: 'B', 12: 'C', 13: 'D', 14: 'E', 15: 'F'}
+        dec_form = self.bin2Dec(num)
+        if dec_form < 10:
+            return str(dec_form)
+        else:
+            return dec_hex_dict[dec_form]
 
     def bin2Dec(self, num: 'str') -> 'int':
-        pass
+        ans = 0
+        for i in range(len(num)):
+            ans += int(num[len(num) - i - 1]) * 2 ** i
+        return ans
 
 def main():
     test = ConcatStr()
     print(test.concatStr("dec", "fab"))
     print(test.concatStr("deca", "fab"))
+    print(test.concatStr("Eqr", "v9oEb12U2ur4xu7rd931G1f50qDo"))
+    ## test decimal to binary
     # print(test.dec2Bin(12))
     # print(test.dec2Bin(1))
     # print(test.dec2Bin(0))
     # print(test.dec2Bin(15))
     # print(test.dec2Bin(16))
+    ## test hexadecimal to binary
     # print(test.hex2Bin("A"))
     # print(test.hex2Bin("b"))
     # print(test.hex2Bin("9"))
     # print(test.hex2Bin("f"))
     # print(test.hex2Bin("0"))
+    ## test binary to decimal
+    # print(test.bin2Dec("0000"))
+    # print(test.bin2Dec("0001"))
+    # print(test.bin2Dec("0101"))
+    # print(test.bin2Dec("1111"))
+    # print(test.bin2Dec("1100"))
+    ## test binary to hexadecimal
+    # print(test.bin2Hex("0000"))
+    # print(test.bin2Hex("0001"))
+    # print(test.bin2Hex("0101"))
+    # print(test.bin2Hex("1111"))
+    # print(test.bin2Hex("1100"))
 
 
 if __name__ == "__main__":
