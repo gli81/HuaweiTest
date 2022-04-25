2767b66a16548790f0cc13249a1f990e50010ae6
gli81 <liguangzheng998@hotmail.com>
2022-04-25 08:35:10 +0800

    completed 037

--- 037/RabbitNum.py
+++ 037/RabbitNum.py
@@ -2,11 +2,15 @@
 
 class RabbitNum():
     def rabbitNum(self, n: 'int') -> 'int':
-        pass
+        if n < 3:
+            return 1
+        else:
+            return self.rabbitNum(n - 2) + self.rabbitNum(n - 1)
 
 def main():
     test = RabbitNum()
     print(test.rabbitNum(3))
+    print(test.rabbitNum(31))
 
 if __name__ == "__main__":
     main()
