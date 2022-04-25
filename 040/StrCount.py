812b8618522b3331040748037d73731f3cd64ce7
gli81 <liguangzheng998@hotmail.com>
2022-04-25 11:39:40 +0800

    completed 040

--- 040/StrCount.py
+++ 040/StrCount.py
@@ -2,11 +2,21 @@
 
 class StrCount():
     def strCount(self, line:'str') -> 'list[int]':
-        pass
+        ct = [0] * 4
+        for char in line:
+            if char.isalpha():
+                ct[0] += 1
+            elif char == ' ':
+                ct[1] += 1
+            elif char.isdigit():
+                ct[2] += 1
+            else:
+                ct[3] += 1
+        return ct
 
 def main():
     test = StrCount()
-    print(test.strCount("1qazxsw23 edcvfr45tgbn hy67uj m,ki89ol.\\/;p0-=\\]["))
+    print(test.strCount("1qazxsw23 edcvfr45tgbn hy67uj m,ki89ol.\\\\/;p0-=\\\\]["))
 
 if __name__ == "__main__":
     main()
