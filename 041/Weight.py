c423e41dd28a5a0bd4502f7a7eb61c72fc2a6215
gli81 <liguangzheng998@hotmail.com>
2022-04-25 12:15:31 +0800

    completed 041

--- 041/Weight.py
+++ 041/Weight.py
@@ -1,8 +1,16 @@
 # -*- coding: utf-8 -*-
 
 class Weight():
-    def weight(self, w: 'list[int]', n: 'list[int]') -> 'int':
-        pass
+    def weight(self, w: 'list[int]', n: 'list[int]') -> 'set[int]':
+
+        ans = [0, ]
+        for i in range(len(w)):
+            temp = ans[:]
+            for j in range(1, n[i] + 1):
+                for elem in temp:
+                    ans.append(elem + w[i] * j)
+                ans = list(set(ans))
+        return ans
 
 def main():
     test = Weight()
