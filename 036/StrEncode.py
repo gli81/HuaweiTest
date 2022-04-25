af26030887c515740cd145ea9512e1ed5cb0f5a0
gli81 <liguangzheng998@hotmail.com>
2022-04-25 08:06:26 +0800

    added 036

---
+++ 036/StrEncode.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class StrEncode():
+    def strEncode(self, key: 'str', line: 'str') -> 'str':
+        pass
+
+def main():
+    test = StrEncode()
+    print(test.strEncode("nihao", "ni"))
+
+if __name__ == "__main__":
+    main()
