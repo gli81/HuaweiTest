ff0461d8ea61426d677bb424ae10ced545c095f3
gli81 <liguangzheng998@hotmail.com>
2022-04-25 11:42:04 +0800

    added 041

---
+++ 041/Weight.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Weight():
+    def weight(self, w: 'list[int]', n: 'list[int]') -> 'int':
+        pass
+
+def main():
+    test = Weight()
+    print(test.weight([1, 2], [2, 1]))
+
+if __name__ == "__main__":
+    main()
