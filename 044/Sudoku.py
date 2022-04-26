e6d01e82062757304dfa0efb82df7d719fa1fd9a
gli81 <liguangzheng998@hotmail.com>
2022-04-26 12:29:50 +0800

    added 044

---
+++ 044/Sudoku.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Sudoku():
+    def sudoku(self):
+        pass
+
+def main():
+    test = Sudoku()
+    print(test.sudoku())
+
+if __name__ == "__main__":
+    main()
