4955deaa9d1423959652fcdadfb046814e4d926a
gli81 <liguangzheng998@hotmail.com>
2022-04-26 16:28:11 +0800

    added 051 053

---
+++ 051/LastButK.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+
+## the distance traveled is set
---
+++ 053/Triangle.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Triangle():
+    def triangle(self, n: "int") -> "int":
+        pass
+
+def main():
+    test = Triangle()
+    print(test.triangle(4))
+
+if __name__ == "__main__":
+    main()
