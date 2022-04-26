88410277ed6bd98cb8e6fff74f479b969bea51dd
gli81 <liguangzheng998@hotmail.com>
2022-04-26 16:34:13 +0800

    completed 053

--- 053/Triangle.py
+++ 053/Triangle.py
@@ -2,11 +2,17 @@
 
 class Triangle():
     def triangle(self, n: "int") -> "int":
-        pass
+        ## -1 -1 2 3 2 4 2 3 2 4...
+        if n < 3: return -1
+        return [2, 3, 2, 4][(n - 3) % 4]
 
 def main():
     test = Triangle()
+    print(test.triangle(3))
     print(test.triangle(4))
+    print(test.triangle(5))
+    print(test.triangle(6))
+    print(test.triangle(7))
 
 if __name__ == "__main__":
     main()
