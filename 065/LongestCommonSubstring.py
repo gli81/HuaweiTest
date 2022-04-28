ae600ab2a4cb9229ab54ee8a6aea9d1271ce7323
gli81 <liguangzheng998@hotmail.com>
2022-04-29 03:17:39 +0800

    added 065

---
+++ 065/LongestCommonSubstring.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class LongestCommonSubstring:
+    def longestCommonSubstring(self, str1: "str", str2: "str") -> "str":
+        pass
+
+def main():
+    test = LongestCommonSubstring()
+    print(test.longestCommonSubstring())
+
+if __name__ == "__main__":
+    main()
