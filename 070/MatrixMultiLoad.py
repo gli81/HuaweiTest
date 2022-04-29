ddb02815e5ce9e07d8d9f6582316226a60b87d08
gli81 <liguangzheng998@hotmail.com>
2022-04-30 00:08:09 +0800

    added 070

---
+++ 070/MatrixMultiLoad.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+class MatrixMultiLoad:
+    def matrixMultiLoad(self, num: "int", order: "str", *lines) -> "int":
+
+        pass
+
+    def loadForTwo(self, dim1: "list[int]", dim2: "list[int]") -> "int":
+        pass
+
+def main():
+    test = MatrixMultiLoad()
+    print(test.matrixMultiLoad(3, "(A(BC))", [50, 10], [10, 20], [20, 5]))
+
+if __name__ == "__main__":
+    main()
