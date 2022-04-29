d2660428678db94930111e046904ad92baac028a
gli81 <liguangzheng998@hotmail.com>
2022-04-30 00:12:35 +0800

    edited 070

--- 070/MatrixMultiLoad.py
+++ 070/MatrixMultiLoad.py
@@ -6,11 +6,12 @@ class MatrixMultiLoad:
         pass
 
     def loadForTwo(self, dim1: "list[int]", dim2: "list[int]") -> "int":
-        pass
+        return dim1[0] * dim1[1] * dim2[1]
 
 def main():
     test = MatrixMultiLoad()
     print(test.matrixMultiLoad(3, "(A(BC))", [50, 10], [10, 20], [20, 5]))
+    print(test.loadForTwo([50, 10], [10, 20]))
 
 if __name__ == "__main__":
     main()
