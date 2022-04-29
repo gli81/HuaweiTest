9d5c7740a04b1f79bd6e6bd8156916f1e554cf34
gli81 <liguangzheng998@hotmail.com>
2022-04-29 23:27:27 +0800

    added 069

---
+++ 069/MatrixMulti.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class MatrixMulti:
+    def matrixMulti(self, m1, n1, n2, nums1, nums2):
+        pass
+
+def main():
+    test = MatrixMulti()
+    print(test.matrixMulti(2, 3, 2, [1, 2, 3, 3, 2, 1], [1, 2, 2, 1, 3, 3]))
+
+if __name__ == "__main__":
+    main()
