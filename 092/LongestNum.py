ef53ee7714460ab312069a10b4da4c700974445b
gli81 <liguangzheng998@hotmail.com>
2022-05-01 23:16:27 +0800

    added 092

---
+++ 092/LongestNum.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+class LongestNum:
+    def longestNum(self, line: "str") -> "str":
+        pass
+
+def main():
+    test = LongestNum()
+    print(test.longestNum("abcd12345ed125ss123058789"))
+    print(test.longestNum("a8a72a6a5yy98y65ee1r2"))
+
+if __name__ == "__main__":
+    main()
