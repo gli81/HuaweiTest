15384eb1fcb2d3baa9429f6b6d73349921e8cecd
gli81 <liguangzheng998@hotmail.com>
2022-05-02 19:35:07 +0800

    added 095

---
+++ 098/Vending.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+
+class Vending:
+    def vending(self):
+        pass
+
+def main():
+    test = Vending()
+    print(test.vending())
+
+if __name__ == "__main__":
+    main()
