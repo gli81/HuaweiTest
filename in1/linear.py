9baadca4892638b1703de16d55dff48c8be14952
gli81 <liguangzheng998@hotmail.com>
2022-05-17 13:42:26 +0800

    interview review

--- in1/linear.py
+++ in1/linear.py
@@ -5,5 +5,36 @@ class Stack:
     def __init__(self):
         self.__elements = list()
 
-    def push(self, elem):
+    def push(self, elem) -> "None":
         self.__elements.append(elem)
+    
+    def pop(self):
+        return self.__elements.pop()
+    
+    def peek(self):
+        return self.__elements[-1]
+    
+    def isEmpty(self) -> "bool":
+        return len(self.__elements) == 0
+    
+    def size(self) -> "int":
+        return len(self.__elements)
+    
+    def __str__(self):
+        return ", ".join(map(str, self.__elements))
+
+class Queue():
+    def __init__(self):
+        self.__items = list()
+    
+    def enqueue(self, item) -> "None":
+        self.__items.append(item)
+    
+    def dequeue(self):
+        return self.__items.pop(0)
+    
+    def size(self) -> "int":
+        return len(self.__items)
+
+    def isEmpty(self) -> "bool":
+        return self.size() == 0
--- in1/main.py
+++ in1/main.py
@@ -4,6 +4,19 @@ import linear
 
 def main():
     stk1 = linear.Stack()
+    print(stk1.isEmpty())
+    stk1.push(2)
+    stk1.push(5)
+    print(stk1)
+    print(stk1.pop())
+    print(stk1)
+    stk1.push(5)
+    stk1.push(7)
+    print(stk1.peek())
+    print(stk1)
+    print(stk1.isEmpty())
+    print(stk1.size())
+    print("=" * 100)
 
 if __name__ == "__main__":
     main()
