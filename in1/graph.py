83dbc3e2abebad9d6dd64817c6f18c2790c9e15f
gli81 <liguangzheng998@hotmail.com>
2022-05-17 18:08:54 +0800

    review graph

--- in1/graph.py
+++ in1/graph.py
@@ -1,7 +1,54 @@
 # -*- coding: utf-8 -*-
 
 class Vertex:
-    pass
+    def _init__(self, key):
+        self.id = key
+        self.connectedTo = {}
+    
+    def addNeighbor(self, nbr, weight = 0):
+        self.connectedTo[nbr] = weight
+    
+    def __str__(self):
+        return str(self.id) + ' connectedTo: ' + str([x.id for x in self.connectedTo])
+    
+    def getConnections(self):
+        return self.conntectedTo.keys()
+    
+    def getId(self):
+        return self.id
+    
+    def getWeight(self, nbr):
+        return self.connectedTo[nbr] 
 
 class Graph:
-    pass
+    def __init__(self):
+        self.vertList = {}
+        self.numVertices = 0
+    
+    def addVertex(self, key):
+        self.numVertices += 1
+        newVertex = Vertex(key)
+        self.vertList[key] = newVertex
+        return newVertex
+    
+    def getVertex(self, n):
+        if n in self.vertList:
+            return self.vertList[n]
+        else:
+            return None
+    
+    def __contains__(self, n):
+        return n in self.vertList
+
+    def addEdge(self, starting, ending, weight = 0):
+        if starting not in self.vertList:
+            nv = self.addVertex(starting)
+        if ending not in self.vertList:
+            nv = self.addVertex(ending)
+        self.vertList[starting].addNeighbor(self.vertList[ending], weight)
+    
+    def getVertices(self):
+        return self.vertList.keys()
+    
+    def __iter__(self):
+        return iter(self.vertList.values())
